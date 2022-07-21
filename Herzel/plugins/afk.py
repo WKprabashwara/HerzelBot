0be3efc68a500425568500c72ad9f62a0188f33b
Rivibibu Praba$hwara <92269011+WKprabashwara@users.noreply.github.com>
2022-07-21 06:56:13 +0530

    Update afk.py

--- Herzel/plugins/afk.py
+++ Herzel/plugins/afk.py
@@ -254,7 +254,7 @@ async def chat_watcher_func(_, message):
         except:
             return
 
-__MODULE__ = "AFK"
+__MODULE__ = "#𝐀𝐅𝐊"
 __HELP__ = """
 When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
 
